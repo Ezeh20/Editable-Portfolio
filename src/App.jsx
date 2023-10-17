3d8f1b15d95031f55333be3ae1aa3f2da294ee4a
Ezeh20 <ranter1570@gmail.com>
2023-10-18 00:49:04 +0100

    chore: setting up routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.scss'
 
 function App() {
+    const uuid = crypto.randomUUID()
+    console.log(uuid)
     return (
         <div>
             <p>ase</p>
-            
         </div>
     )
 }
