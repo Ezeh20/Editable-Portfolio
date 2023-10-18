7730cd9f8cf9db3e584929bafd7abfcc9b9a113a
Ezeh20 <ranter1570@gmail.com>
2023-10-18 21:02:04 +0100

    working on the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Signup from './pages/signup'
 import Login from './pages/login'
+import Portfolio from './pages/portfolio'
 
 function App() {
     return (
         <Routes>
             <Route index element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
     )
 }
---
+++ src/pages/portfolio/Portfolio.jsx
@@ -0,0 +1,17 @@
+import About from './sections/about'
+import Contact from './sections/contact'
+import Experience from './sections/experience'
+import Introduction from './sections/introduction'
+import Works from './sections/works'
+
+export function Portfolio() {
+    return (
+        <div>
+            <Introduction />
+            <About />
+            <Experience />
+            <Works />
+            <Contact />
+        </div>
+    )
+}
