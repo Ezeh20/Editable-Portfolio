418e6fbfc2f37958a85af598b1b187b89cb3b445
Ezeh20 <ranter1570@gmail.com>
2023-10-19 01:38:40 +0100

    made an active route based navigation component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,40 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
     },
-    extends: [
-        'airbnb',
-        'airbnb/hooks',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'import/prefer-default-export': 0,
+    'no-console': 0,
+    'no-nested-ternary': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
     ],
-    overrides: [
-        {
-            env: {
-                node: true,
-            },
-            files: ['.eslintrc.{js,cjs}'],
-            parserOptions: {
-                sourceType: 'script',
-            },
-        },
-    ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react', 'prettier'],
-    rules: {
-        'react/react-in-jsx-scope': 0,
-        'import/prefer-default-export': 0,
-        'no-console': 0,
-        'no-nested-ternary': 0,
-        'prettier/prettier': [
-            'error',
-            {
-                endOfLine: 'auto',
-            },
-        ],
-    },
+  },
 }
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,52 +1,79 @@
 import { Link, useLocation } from 'react-router-dom'
+import { AiOutlineClose } from 'react-icons/ai'
+import { RiAppsLine } from 'react-icons/ri'
+import { useState } from 'react'
 import Container from '../container'
 import { constants } from './constants'
 import styles from './Navigation.module.scss'
 
 export function Navigation() {
-    const location = useLocation()
-    const login = location.pathname === '/'
-    const signup = location.pathname === '/signup'
-    const portfolio = location.pathname === '/portfolio'
+  const location = useLocation()
+  const [toggle, setToggle] = useState(false)
+  const login = location.pathname === '/'
+  const signup = location.pathname === '/signup'
+  const portfolio = location.pathname === '/portfolio'
 
-    const navigate = () => {
-        if (login) {
-            return '/signup'
-        }
-        if (signup) {
-            return '/'
-        }
-        return null
+  const navigate = () => {
+    if (login) {
+      return '/signup'
     }
+    if (signup) {
+      return '/'
+    }
+    return null
+  }
 
-    return (
-        <header className={styles.header}>
-            <Container>
-                <nav className={styles.nav}>
-                    <p className={styles.logo}>{constants.logo}</p>
-                    {portfolio ? (
-                        <div>
-                            <ul className={styles.ul}>
-                                {constants.links.map((itm) => {
-                                    const { id, text, to } = itm
-                                    return (
-                                        <li key={id} className={styles.li}>
-                                            <Link to={to} className={styles}>
-                                                {text}
-                                            </Link>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                            <div className={styles.user}>user</div>
-                        </div>
-                    ) : (
-                        <Link to={navigate()} className={styles.link}>
-                            {login ? 'Signup' : signup ? 'Login' : undefined}
+  return (
+    <header className={styles.header}>
+      <Container>
+        <nav className={styles.nav}>
+          <p className={styles.logo}>{constants.logo}</p>
+          {portfolio ? (
+            <div className={styles.menu}>
+              <div
+                className={
+                  toggle
+                    ? `${styles.portfolio} ${styles.portfolioAlt}`
+                    : `${styles.portfolio}`
+                }
+              >
+                <ul className={styles.ul}>
+                  {constants.links.map((itm) => {
+                    const { id, text, to } = itm
+                    return (
+                      <li key={id} className={styles.li}>
+                        <Link
+                          to={to}
+                          className={styles.list}
+                          onClick={() => setToggle((pre) => !pre)}
+                        >
+                          {text}
                         </Link>
-                    )}
-                </nav>
-            </Container>
-        </header>
-    )
+                      </li>
+                    )
+                  })}
+                </ul>
+                <div className={styles.user}>user</div>
+              </div>
+              {toggle ? (
+                <AiOutlineClose
+                  onClick={() => setToggle((pre) => !pre)}
+                  className={styles.toggle}
+                />
+              ) : (
+                <RiAppsLine
+                  onClick={() => setToggle((pre) => !pre)}
+                  className={styles.toggle}
+                />
+              )}
+            </div>
+          ) : (
+            <Link to={navigate()} className={styles.link}>
+              {login ? 'Signup' : signup ? 'Login' : undefined}
+            </Link>
+          )}
+        </nav>
+      </Container>
+    </header>
+  )
 }
