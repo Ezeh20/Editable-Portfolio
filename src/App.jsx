d9fd66879488ffb76857d8dc1b1914962348fe1e
Ezeh20 <ranter1570@gmail.com>
2023-10-19 15:19:25 +0100

    staging

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import Signup from './pages/signup'
 import Login from './pages/login'
 import Portfolio from './pages/portfolio'
 
+
 function App() {
-    return (
-        <Routes>
-            <Route index element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route index element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+    </Routes>
+  )
 }
 
 export default App
