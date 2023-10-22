23485706601eeee60df316054bfe21a1270c24c9
Ezeh20 <ranter1570@gmail.com>
2023-10-22 16:40:42 +0100

    refactor: refactored the editableContent component

--- src/components/editableContent/editableContent.jsx
+++ src/components/editableContent/editableContent.jsx
@@ -1,105 +1,76 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import PropTypes from 'prop-types'
 import React, { useState, useRef } from 'react'
+import ContentEditable from 'react-contenteditable'
 
 export function EditableElement({ initialValue, maxWords, tag, className }) {
   const [isEditing, setIsEditing] = useState(false)
   const [value, setValue] = useState(initialValue)
-  const contentRef = useRef(null)
+  const tapped = useRef(0)
 
-  // The makes sure the cursor is always at the front of the word
-  const alignCursor = (element) => {
-    const range = document.createRange()
-    const selection = window.getSelection()
-    range.selectNodeContents(element)
-    range.collapse(false)
-    selection.removeAllRanges()
-    selection.addRange(range)
-    element.focus()
-  }
-
-  // enable editing on double click
-  const handleStartEditing = () => {
+  const startEditing = () => {
     setIsEditing(true)
-    setTimeout(() => {
-      alignCursor(contentRef.current)
-    }, 0)
   }
 
   const handleDoubleClick = () => {
-    handleStartEditing()
+    startEditing()
   }
 
-  // enable editing on touch devices
-  const handleTouchStart = (e) => {
-    if (!isEditing) {
-      e.preventDefault() // Prevent the default touch action
-      handleStartEditing()
+  /**
+   * enable double tap on touch devices
+   * use the Date method to keep track of tap instances
+   * if the instance - the tapped current value is less than 300, enable editing
+   */
+  const handleTouchStart = () => {
+    const instance = new Date().getTime()
+    const delay = 300
+
+    if (instance - tapped.current < delay) {
+      startEditing()
     }
+    tapped.current = instance
   }
-
+  // stops editing when focus is moved
   const handleBlur = () => {
     setIsEditing(false)
   }
 
-  // update the text content while taking note of the max amount of words
+  // Stop editing when the "Enter" key is pressed
+  const handleEnterKey = (e) => {
+    if (e.key === 'Enter') {
+      handleBlur()
+    }
+  }
   const handleChange = (e) => {
-    const newValue = e.target.textContent
+    const newValue = e.target.value
     const words = newValue.split(/\s+/)
     if (words.length <= maxWords) {
       setValue(newValue)
     } else {
-      e.preventDefault() // Prevent further input
       setIsEditing(false)
-      contentRef.current.textContent = value
     }
   }
 
-  // Monitors key press; then calls the alignCursor
-  // function to Move the cursor to the end of the word
-  const handleKeyDown = (event) => {
-    if (!isEditing) {
-      return
-    }
-    // Stops editing if the enter key is pressed
-    if (event.key === 'Enter') {
-      event.preventDefault()
-      setIsEditing(false)
-    }
-    alignCursor(contentRef.current)
-  }
-
-  const dirValue = isEditing ? 'ltr' : 'auto'
   const wordCount = value.split(/\s+/).length
-  const showWordCount = `${wordCount}/${maxWords} word(s)`
-
-  const Element = React.createElement(
-    tag,
-    {
-      ref: contentRef,
-      onDoubleClick: handleDoubleClick,
-      onTouchStart: handleTouchStart,
-      onBlur: handleBlur,
-      contentEditable: isEditing,
-      suppressContentEditableWarning: true,
-      onInput: handleChange,
-      onKeyDown: handleKeyDown,
-      style: {
-        display: 'inline',
-        unicodeBidi: 'plaintext',
-        direction: dirValue,
-      },
-      className,
-    },
-    value
-  )
 
   return (
     <>
-      {Element}
+      <ContentEditable
+        tagName={tag}
+        html={value}
+        disabled={!isEditing}
+        onChange={handleChange}
+        onDoubleClick={handleDoubleClick}
+        onTouchStart={(e) => handleTouchStart(e)}
+        onBlur={handleBlur}
+        onKeyDown={handleEnterKey}
+        className={className}
+        style={{ cursor: 'pointer', touchAction: 'none' }}
+      />
       {isEditing && (
-        <span style={{ fontSize: '16px', color: '#ccd6f6' }}>
-          {showWordCount}
+        <span style={{ color: 'var(--white)' }}>
+          {wordCount}/{maxWords} word(s)
         </span>
       )}
     </>
