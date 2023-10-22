f58bdcba36949c8e01835dca174d3715971c4002
Ezeh20 <ranter1570@gmail.com>
2023-10-22 01:16:25 +0100

    ...

--- src/pages/portfolio/sections/introduction/constants.js
+++ src/pages/portfolio/sections/introduction/constants.js
@@ -4,8 +4,8 @@ export const constants = {
   greeting: 'Hi my name is',
   name: 'John Doe',
   field: 'A Frontend Developer',
-  content: `This is a random text that can 
-  be edited as you please. Just give
+  content: `This is a random text that is  
+  editable in-line. Just give
   a warm and engaging intro. Introduce yourself, what you have been up to and what you are looking forward too. Don't talk about your 
   cat or anything like that; just give a quick intro about yourself and endeavors.`,
   img: hero,
